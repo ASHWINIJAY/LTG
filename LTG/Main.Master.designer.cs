414eac1505e9ced2e66aba793652567ce0bbafee
Aswini <ashujay20@gmail.com>
2026-05-07 10:26:17 +0200

    LTG Changes

--- LTG/Main.Master.designer.cs
+++ LTG/Main.Master.designer.cs
@@ -258,6 +258,15 @@ namespace LTG
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl LiDeliveryNoteReprint;
 
         /// <summary>
+        /// LiReGenerateDN control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl LiReGenerateDN;
+
+        /// <summary>
         /// LiInboundException control.
         /// </summary>
         /// <remarks>
